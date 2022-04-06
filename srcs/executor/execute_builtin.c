b03386d45d128032405e03f0d0f329ab95cdef60
donutkre <kim@DESKTOP-BB84EU9.localdomain>
2022-04-06 16:35:54 +0800

    fixed exit

--- srcs/executor/execute_builtin.c
+++ srcs/executor/execute_builtin.c
@@ -28,13 +28,6 @@ int	execute_builtin(t_mini *mini)
 		builtin_export(mini->cmd_args, mini->env_head);
 	else if (!ft_strncmp(mini->cmd_args[0], "exit", 5))
 		builtin_exit(mini->cmd_args, mini->env_head);
-	// else if (ft_strncmp(mini->cmd_args[0], "$?", 2))
-	// {
-	// 	printf("%d: command not found\n", mini->exit_status);
-	// 	mini->exit_status = 127;
-	// 	dup2(mini->write, 1);
-	// 	return (0);
-	// }
 	else
 		return (0);
 	return (1);

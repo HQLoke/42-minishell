314f39bfbac17a71e342b2fed3556c581bd28b30
donutkre <kim@DESKTOP-BB84EU9.localdomain>
2022-04-12 14:35:21 +0800

    added exit int max

--- srcs/builtin/exit.c
+++ srcs/builtin/exit.c
@@ -16,29 +16,74 @@
 	Prints exit and error message. 
 	minishell is closed and the exit code and terminates it.
 */
+static int	check_int_max(int sign, char *arg, long long n)
+{
+	int len;
+
+    len = ft_isdigit(arg[1]);
+    if (len == 0)
+		return (0);
+    n *= sign;
+	if (n > LONG_MAX / 10 || (n == LONG_MAX / 10 && arg[1] > '7'))
+        return (1);
+    else if (n < LONG_MIN / 10 || (n == LONG_MIN / 10 && arg[1] > '8'))
+        return (1);
+	return (0);
+}
+
+static int	atoi_exit(char *arg)
+{
+	int			sign;
+	long long	i;
+
+	sign = 1;
+	i = 0;
+	while ((*arg >= '\t' && *arg <= '\r') || *arg == ' ')
+		arg++;
+	if (*arg == '-')
+	{
+		sign *= -1;
+		arg++;
+	}
+	else if (*arg == '+')
+		arg++;
+	while (*arg && ft_isdigit(*arg))
+	{
+        if (i)
+			i *= 10;
+		i += *arg - '0';
+		if (check_int_max(sign, arg, i) == 1)
+			return (0);
+		arg++;
+	}
+	if (*arg != '\0')
+		return (0);
+    return (1);
+}
+
 int	builtin_exit(t_cmd *node)
 {
-	int	status;
-
-	status = 0;
+	unsigned char	status;
 	
-	if (status == 1)
+	status = 0;
+	if (node->cmd_args[1])
 	{
-		ft_putendl_fd("exit", 1);
-		exit(EXIT_SUCCESS);
+		if (atoi_exit(node->cmd_args[1]))
+		{
+			status = ft_atoi(node->cmd_args[1]);
+		}
+			
+		else
+		{
+			error_numeric_argument(node->cmd_args[1]);
+			ft_putstr_fd("bash: exit: ", 2);
+			ft_putstr_fd(node->cmd_args[1], 2);
+			ft_putstr_fd(": numeric argument required\n", 2);
+			status = 255;
+		}
 	}
-	if (status > 2)
-	{
-		ft_putendl_fd("bash: exit: too many arguments", 2);
-		return (1);
-	}
-	if (ft_atoi(cmd[1]) == 0)
-	{
-		ft_putstr_fd("bash: exit: ", 2);
-		ft_putstr_fd(cmd[1], 2);
-		ft_putstr_fd(": numeric argument required\n", 2);
-		exit(255);
-	}
-	ft_putstr_fd("exit\n", 1);
-	exit(ft_atoi(cmd[1]));
+	ft_putstr_fd("bash: exit: ", 2);
+	ft_putstr_fd(status, 2);
+	ft_putstr_fd("\n", 2);
+	return(status);
 }

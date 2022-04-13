c9e5a31fb75b161dca10ec029e951f4c6017f674
donutkre <kim@DESKTOP-BB84EU9.localdomain>
2022-04-13 13:13:54 +0800

    fixed cmd error

--- srcs/utils/ft_error_exit.c
+++ srcs/utils/ft_error_exit.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_error_exit.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hloke <hloke@student.42kl.edu.my>          +#+  +:+       +#+        */
+/*   By: ktiong <ktiong@student.42kl.edu.my>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/07 16:33:43 by hloke             #+#    #+#             */
-/*   Updated: 2022/04/07 17:13:20 by hloke            ###   ########.fr       */
+/*   Updated: 2022/04/13 13:12:19 by ktiong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,4 +16,13 @@ void	ft_error_exit(char *error_msg)
 {
 	ft_putstr_fd(error_msg, STDERR_FILENO);
 	exit (EXIT_FAILURE);
+}
+
+void	cmd_not_found(char *error_msg)
+{
+	ft_putstr_fd("minishell: ", 2);
+	ft_putstr_fd(error_msg, 2);
+	ft_putstr_fd(": ", 2);
+	ft_putstr_fd("command not found\n", 2);
+	exit(127);
 }
